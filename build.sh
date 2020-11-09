558ce70cec6e15aafb2ffea269395a230286143f
Yorling <shallowcloud@yeah.net>
2020-11-09 14:58:30 +0800

    fix: copy config file example to build artifacts

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ FLAGS="-X 'main.compiledTimeString=$(date --rfc-3339='seconds')' -X main.version
 
 mkdir -p build/bin build/conf
 cp scripts/bootstrap.sh scripts/omil.service.template scripts/install.sh build/ 2>/dev/null
+cp conf/example.config.yml build/conf/ 2>/dev/null
 
 chmod +x build/bootstrap.sh
 
